9559c53e01a3490b1e6ad46ff09ffa889f8c3984
Ian Moon <ianmoon20@gmail.com>
2024-03-20 23:52:33 -0400

    Adding tests for textnode to htmlnode parsing

--- src/tests/test_textnode.py
+++ src/tests/test_textnode.py
@@ -9,17 +9,30 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", "bold")
         self.assertEqual(node, node2)
 
+    def test_no_props_eq(self):
         node = TextNode("This is a text node", "bold", None)
         node2 = TextNode("This is a text node", "bold", None)
         self.assertEqual(node, node2)
 
+    def test_prop_eq(self):
         node = TextNode(None, "bold", "Test URL")
         node2 = TextNode(None, "bold", "Test URL")
+        self.assertEqual(node, node2)
+    
+    def test_props_eq(self):
+        node = TextNode(None, "bold", {"src": "Test URL"})
+        node2 = TextNode(None, "bold", {"src": "Test URL"})
         self.assertEqual(node, node2)
 
     def test_text_text_node_to_html_node(self):
         node = text_node_to_html_node(TextNode("Hi", "text"))
         assert node.tag is None
+        assert node.value == "Hi"
+        assert node.props is None
+
+    def test_bold_text_node_to_html_node(self):
+        node = text_node_to_html_node(TextNode("Hi", "bold"))
+        assert node.tag is "b"
         assert node.value == "Hi"
         assert node.props is None
 
